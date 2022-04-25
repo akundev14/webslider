198f349de9222f3021396dec7b4135877f483a17
Akundev14 <akundeveloper14@gmail.com>
2022-04-25 14:22:36 +0700

    update rename image name

--- slider/admin/tambahdata.php
+++ slider/admin/tambahdata.php
@@ -15,14 +15,15 @@ $sumber = $_FILES['gambar']['tmp_name'];
     $target = 'images/';
 
     $nama_gambar = $_FILES['gambar']['name'];
+    $namabaru = date('dmYHis-').$nama_gambar;
 
 
 
-$pindah = move_uploaded_file($sumber, $target.$nama_gambar);
+$pindah = move_uploaded_file($sumber, $target.$namabaru);
 
 if($pindah){
 
-$query = "insert into carousel values('','$judul','$desk','$nama_gambar')";
+$query = "insert into carousel values('','$judul','$desk','$namabaru')";
 
 $hasil =$connection->query($query);
 
